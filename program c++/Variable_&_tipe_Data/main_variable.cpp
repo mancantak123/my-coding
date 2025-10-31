8905b8f44f0e60fa9e4853a64351d6589ff28ef7
mancantak123 <nikosampit75@gmail.com>
2025-10-31 13:21:32 +0700

    hello world

---
+++ program c++/Variable_&_tipe_Data/main_variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int umur = 12;
+    string nama = "hallo dunia";
+
+    cout << "umur : \t" << umur<< endl;
+    cout << "string : \t" << nama <<endl;
+}
