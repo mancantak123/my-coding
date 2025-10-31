cbdbb010431e06895effc910031cd7345032d4f8
mancantak123 <nikosampit75@gmail.com>
2025-10-31 13:58:39 +0700

    hello world

---
+++ program c++/Perulangan/perulangan_dasar/latihan/Latihan_While.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i,a;
+
+    cout << "Nilai  : ";
+    cin >> i;
+    cout << "nilai batas : ";
+    cin >> a;
+    while (i <= a)
+    {
+        cout << "perulangan ke-"<< i << endl;
+        i++;
+    }
+    
+}
---
+++ program c++/Perulangan/perulangan_dasar/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int i = 1;
+
+    while ( i <= 10)
+    {
+        cout << "ini perulangan ke-"<< i <<endl;
+        i++;
+    }
+    
+}
