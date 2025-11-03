0a72e9a66068edc4b00335684365ea261a60b114
mancantak123 <nikosampit75@gmail.com>
2025-11-03 11:54:38 +0700

    hello world

---
+++ program c++/Perulangan/mencari pola/mencari pola Ⅶ/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=10;
+    
+    
+    for(int i=1; i<=n;i++){
+        for(int j=1; j < i; j++){
+            cout<<"-";
+        }
+        
+        for(int k=n; k >= (2*i-n);k--){
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+}
