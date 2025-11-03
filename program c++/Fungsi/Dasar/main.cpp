b0ea8c75b0593cf65624d5be58e6b9d8007d33c5
mancantak123 <nikosampit75@gmail.com>
2025-11-03 13:36:09 +0700

    hello world

---
+++ program c++/Fungsi/Dasar/main.cpp
@@ -0,0 +1,13 @@
+//program dasar sebuah fungsi di c++
+
+#include<iostream>
+using namespace std;
+
+void cetakSalam(){
+    cout<<"Hello, selamat datang di program fungsi!"<<endl;
+}
+
+int main(){
+    cetakSalam();
+    return 0;
+}
