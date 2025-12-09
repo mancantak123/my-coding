7540a646bd9b499386e46e889ddce449664cdd9d
mancantak123 <nikosampit75@gmail.com>
2025-12-09 16:29:26 +0700

    Refactor code structure for improved readability and maintainability

---
+++ program c++/objek oriented programing/inheritance/inheritance Dasar/main.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Siswa{
+    private:
+        string nama;
+        int umur;
+        
+        void setNilai(int n){
+            if(n < 18 && n > 15){
+                umur = n;
+            }else{
+                cout<<"tidak valid!!"<<endl;
+            }
+        }
+        
+        
+    public:
+        Siswa(string na , int u){
+            nama = na;
+            setNilai(u);
+        }
+    
+    void infoDasar(){
+        cout<<"Nama :"<<nama<<endl;
+        cout<<"umur :"<<umur<<endl;
+    }
+};
+
+class SiswaSma : public Siswa{
+    private:
+        string jurusan;
+        
+        void setJurusan(string ju){
+            if(ju == "IPA"){
+                jurusan = "IPA";
+                
+            }else if(ju == "IPS" ){
+                jurusan = "IPS";
+                
+            }else{
+                cout<<"tidak valid"<<endl;
+            }
+        }
+    public:
+        SiswaSma(string name , int age, string major )
+            : Siswa(name , age) {
+                setJurusan(major);
+            }
+        
+        void info(){
+            infoDasar();
+            cout<<"Jurusan :"<<jurusan<<endl;
+        }
+};
+
+int main(){
+    SiswaSma SISWA1("Udin" , 17 , "IPA");
+    SISWA1.info();
+    
+    cin.get();
+}
+
+
+
