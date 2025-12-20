4aae0b59c01708ca718edc847581c83fc7a5e36b
mancantak123 <nikosampit75@gmail.com>
2025-12-20 14:50:14 +0700

    Refactor code structure for improved readability and maintainability

---
+++ program cpp/Struktur Data/Pointer & Reference/Dasar Reference/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int p;
+    cin>>p;
+    
+    // reference !!
+    // reference adalah alamat sama yang menyimpan tambahan nama lainnya!!
+    int &r = p;
+    
+    cout<<&r<<endl;
+    cout<<&p<<endl;
+    
+
+    cin.get();
+}
