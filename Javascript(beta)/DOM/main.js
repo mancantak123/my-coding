8cf54d0f34e7baaa58b8414e5694952d1c0fad4b
mancantak123 <nikosampit75@gmail.com>
2026-06-05 05:42:30 +0700

    Add initial HTML and JavaScript files for DOM manipulation examples

---
+++ Javascript(beta)/DOM/main.js
@@ -0,0 +1,10 @@
+document.title = "document objek model"
+
+const body = document.body
+
+body.append('helllo world')
+
+const h1 = document.createElement('h1') // inisiasi element html
+h1.textContent = 'hello bro'    // inisiasi value element html
+
+body.append(h1) // menambahkan element html yang dibuat js ke html
---
+++ Javascript(beta)/javascript_basic/main.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+
+function loop(){
+    for(let angka = 0; angka < 10;angka++){
+        console.log("perulangan ke-" , angka)
+    }
+}
+
+loop()
